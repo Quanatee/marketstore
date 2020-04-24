712397b6f348e7f3303260f75c7654831ae9679b
Raymond Luo <rymdluo@users.noreply.github.com>
2020-04-25 01:45:12 +0800

    Update pentagon.go

--- quanatee/contrib/pentagon/pentagon.go
+++ quanatee/contrib/pentagon/pentagon.go
@@ -13,7 +13,7 @@ import (
 	"github.com/alpacahq/marketstore/executor"
 	"github.com/alpacahq/marketstore/planner"
 	"github.com/alpacahq/marketstore/plugins/bgworker"
-	"github.com/alpacahq/marketstore/utils"
+	//"github.com/alpacahq/marketstore/utils"
 	"github.com/alpacahq/marketstore/utils/io"
 	"github.com/alpacahq/marketstore/utils/log"
 )
