ec4800e185086c47368538fd3af8002b93ed2bcd
Raymond Luo <rymdluo@users.noreply.github.com>
2020-04-25 20:08:42 +0800

    Update backfill.go

--- quanatee/contrib/pentagon/backfill/backfill.go
+++ quanatee/contrib/pentagon/backfill/backfill.go
@@ -9,7 +9,7 @@ import (
 	"github.com/alpacahq/marketstore/quanatee/contrib/pentagon/api4polygon"
 	"github.com/alpacahq/marketstore/executor"
 	"github.com/alpacahq/marketstore/utils/io"
-	//"github.com/alpacahq/marketstore/utils/log"
+	"github.com/alpacahq/marketstore/utils/log"
 )
 
 var (
