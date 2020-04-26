103854e8bdb5cc9f0cffbdac0d8bc6db970a0255
Raymond Luo <rymdluo@users.noreply.github.com>
2020-04-26 15:08:47 +0800

    Update livefill.go

--- quanatee/contrib/pentagon/livefill/livefill.go
+++ quanatee/contrib/pentagon/livefill/livefill.go
@@ -19,7 +19,7 @@ var (
 
 func Bars(symbol string, from, to time.Time) (err error) {
 	if from.IsZero() {
-		from = time.Date(2017, 1, 1, 0, 0, 0, 0, time.UTC
+		from = time.Date(2017, 1, 1, 0, 0, 0, 0, time.UTC)
 	}
 
 	if to.IsZero() {
