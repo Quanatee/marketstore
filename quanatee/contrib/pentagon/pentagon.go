01a42e315e78e21b6f55dd5458bd3ac1adb758fa
Raymond Luo <rymdluo@users.noreply.github.com>
2020-04-28 19:03:33 +0800

    Update pentagon.go

--- quanatee/contrib/pentagon/pentagon.go
+++ quanatee/contrib/pentagon/pentagon.go
@@ -151,7 +151,7 @@ func (qf *QuanateeFetcher) Run() {
 
 func (qf *QuanateeFetcher) workBackfillBars() {
 
-	ticker := time.NewTicker(30 * time.Second)
+	ticker := time.NewTicker(60 * time.Second)
 
 	for range ticker.C {
 		
