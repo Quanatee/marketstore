4dddc511498179e2ed5955b57701eacee1d549cc
Raymond Luo <rymdluo@users.noreply.github.com>
2020-04-29 04:24:19 +0800

    Update aggtrigger.go

--- quanatee/contrib/pentaggregation/aggtrigger/aggtrigger.go
+++ quanatee/contrib/pentaggregation/aggtrigger/aggtrigger.go
@@ -287,12 +287,12 @@ func aggregate(cs *io.ColumnSeries, tbk *io.TimeBucketKey) *io.ColumnSeries {
 	if cs.Exists("HLC") {
 		params = append(params, accumParam{"HLC", "avg", "HLC"})
 		params = append(params, accumParam{"HLC", "roc", "ROC"}) // Original output
-		params = append(params, accumParam{"HLC", "std", "VOL"}) // Original Output
+		params = append(params, accumParam{"HLC", "std", "STD"}) // Original Output
     }
 	if cs.Exists("TVAL") {
 		params = append(params, accumParam{"TVAL", "sum", "TVAL"})
 		params = append(params, accumParam{"TVAL", "roc", "TROC"}) // Original output
-		params = append(params, accumParam{"TVAL", "std", "TVOL"}) // Original Output
+		params = append(params, accumParam{"TVAL", "std", "TSTD"}) // Original Output
     }
 	if cs.Exists("Spread") {
 		params = append(params, accumParam{"Spread", "avg", "Spread"})
