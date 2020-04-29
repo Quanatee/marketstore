132b03350e791a43c701bbd94a1c8bfafcb796d6
Raymond Luo <rymdluo@users.noreply.github.com>
2020-04-29 23:58:58 +0800

    Update filler.go

--- quanatee/contrib/pentagon/filler/filler.go
+++ quanatee/contrib/pentagon/filler/filler.go
@@ -56,7 +56,7 @@ func Bars(symbol, marketType string, from, to time.Time) (err error) {
 	if len(ohlcv.HLC) > 0 {
 		// Randomly run alt providers to ease api usage
 		rand.Seed(GetRandSeed())
-		if rand.Intn(9) >= 3 {
+		if rand.Intn(3) >= 1 {
 			ohlcv_ti := GetDataFromProvider("tiingo", symbol, marketType, from, to)
 			if len(ohlcv_ti.HLC) > 0 {
 				ohlcvs = append(ohlcvs, ohlcv_ti)
@@ -65,7 +65,7 @@ func Bars(symbol, marketType string, from, to time.Time) (err error) {
 			}
 		}
 		rand.Seed(GetRandSeed())
-		if rand.Intn(9) <= 7 {
+		if rand.Intn(3) >= 1 {
 			ohlcv_tw := GetDataFromProvider("twelve", symbol, marketType, from, to)
 			if len(ohlcv_tw.HLC) > 0 {
 				ohlcvs = append(ohlcvs, ohlcv_tw)
@@ -106,7 +106,7 @@ func Bars(symbol, marketType string, from, to time.Time) (err error) {
 			}
 		}
 		rand.Seed(GetRandSeed())
-		if rand.Intn(3) <= 1 {
+		if rand.Intn(3) >= 1 {
 			ohlcv_twb := GetDataFromProvider("twelve", symbol[:len(symbol)-3] + "B" + symbol[len(symbol)-3:], marketType, from, to)
 			if len(ohlcv_twb.HLC) > 0 {
 				ohlcvs = append(ohlcvs, ohlcv_twb)
@@ -131,7 +131,7 @@ func Bars(symbol, marketType string, from, to time.Time) (err error) {
 			}
 		}
 		rand.Seed(GetRandSeed())
-		if rand.Intn(3) <= 1 {
+		if rand.Intn(3) >= 1 {
 			ohlcv_twt := GetDataFromProvider("twelve", symbol+"T", marketType, from, to)
 			if len(ohlcv_twt.HLC) > 0 {
 				ohlcvs = append(ohlcvs, ohlcv_twt)
@@ -147,7 +147,7 @@ func Bars(symbol, marketType string, from, to time.Time) (err error) {
 			log.Debug("Adding Polygon USDC %s to %v", symbol, len(ohlcvs))
 		}
 		rand.Seed(GetRandSeed())
-		if rand.Intn(3) >= 0 {
+		if rand.Intn(3) >= 1 {
 			ohlcv_tic := GetDataFromProvider("tiingo", symbol+"C", marketType, from, to)
 			if len(ohlcv_tic.HLC) > 0 {
 				ohlcvs = append(ohlcvs, ohlcv_tic)
@@ -156,7 +156,7 @@ func Bars(symbol, marketType string, from, to time.Time) (err error) {
 			}
 		}
 		rand.Seed(GetRandSeed())
-		if rand.Intn(3) <= 1 {
+		if rand.Intn(3) >= 1 {
 			ohlcv_twc := GetDataFromProvider("twelve", symbol+"C", marketType, from, to)
 			if len(ohlcv_twc.HLC) > 0 {
 				ohlcvs = append(ohlcvs, ohlcv_twc)
