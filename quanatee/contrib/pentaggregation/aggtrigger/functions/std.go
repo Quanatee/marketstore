a88790a4d2ca18d278a2f374caad3f55eba7e6f0
Raymond Luo <rymdluo@users.noreply.github.com>
2020-04-30 23:34:47 +0800

    Update std.go

--- quanatee/contrib/pentaggregation/aggtrigger/functions/std.go
+++ quanatee/contrib/pentaggregation/aggtrigger/functions/std.go
@@ -5,16 +5,16 @@ import "math"
 func StdFloat32(values []float32) float32 {
 
 	upcasted_values := convertFloat32ToFloat64(values)
-	std := StdDev(upcasted_values, nil)
+	std, _ := StandardDeviation(upcasted_values)
+	
 	return float32(std)
-
 }
 
 func StdFloat64(values []float64) float64 {
 
-	std := StdDev(values, nil)
-	return std
+	std, _ := StandardDeviation(values)
 
+	return std
 }
 
 func convertFloat32ToFloat64(ar []float32) []float64 {
@@ -36,77 +36,88 @@ func convertFloat64ToFloat32(ar []float64) []float32 {
 	return newar
  }
 
-// StdDev returns the sample standard deviation.
-func StdDev(x, weights []float64) float64 {
-	_, std := MeanStdDev(x, weights)
-	return std
+// adopted from https://github.com/montanaflynn/stats with slight tweaks
+
+type statsError struct {
+	err string
 }
 
-// MeanStdDev returns the sample mean and standard deviation
-func MeanStdDev(x, weights []float64) (mean, std float64) {
-	mean, variance := MeanVariance(x, weights)
-	return mean, math.Sqrt(variance)
+func (s statsError) Error() string {
+	return s.err
 }
 
-// Mean computes the weighted mean of the data set.
-//  sum_i {w_i * x_i} / sum_i {w_i}
-// If weights is nil then all of the weights are 1. If weights is not nil, then
-// len(x) must equal len(weights).
-func Mean(x, weights []float64) float64 {
-	if weights == nil {
-		return floats.Sum(x) / float64(len(x))
-	}
-	if len(x) != len(weights) {
-		panic("stat: slice length mismatch")
-	}
-	var (
-		sumValues  float64
-		sumWeights float64
-	)
-	for i, w := range weights {
-		sumValues += w * x[i]
-		sumWeights += w
-	}
-	return sumValues / sumWeights
+func (s statsError) String() string {
+	return s.err
 }
 
-// MeanVariance computes the sample mean and variance, where the mean and variance are
-//  \sum_i w_i * x_i / (sum_i w_i)
-//  \sum_i w_i (x_i - mean)^2 / (sum_i w_i - 1)
-// respectively.
-// If weights is nil then all of the weights are 1. If weights is not nil, then
-// len(x) must equal len(weights).
-func MeanVariance(x, weights []float64) (mean, variance float64) {
+var (
+	EmptyInputErr = statsError{"Input must not be empty."}
+)
 
-	// This uses the corrected two-pass algorithm (1.7), from "Algorithms for computing
-	// the sample variance: Analysis and recommendations" by Chan, Tony F., Gene H. Golub,
-	// and Randall J. LeVeque.
+// StandardDeviation the amount of variation in the dataset
+func StandardDeviation(input []float64) (sdev float64, err error) {
+	return StandardDeviationPopulation(input)
+}
 
-	// note that this will panic if the slice lengths do not match
-	mean = Mean(x, weights)
-	var (
-		ss           float64
-		compensation float64
-	)
-	if weights == nil {
-		for _, v := range x {
-			d := v - mean
-			ss += d * d
-			compensation += d
-		}
-		variance = (ss - compensation*compensation/float64(len(x))) / float64(len(x)-1)
-		return
+// StandardDeviationPopulation finds the amount of variation from the population
+func StandardDeviationPopulation(input []float64) (sdev float64, err error) {
+
+	if len(input) == 0 {
+		return math.NaN(), EmptyInputErr
 	}
 
-	var sumWeights float64
-	for i, v := range x {
-		w := weights[i]
-		d := v - mean
-		wd := w * d
-		ss += wd * d
-		compensation += wd
-		sumWeights += w
+	// Get the population variance
+	vp, _ := PopulationVariance(input)
+
+	// Return the population standard deviation
+	return math.Pow(vp, 0.5), nil
+}
+
+
+// PopulationVariance finds the amount of variance within a population
+func PopulationVariance(input []float64) (pvar float64, err error) {
+
+	v, err := _variance(input, 0)
+	if err != nil {
+		return math.NaN(), err
 	}
-	variance = (ss - compensation*compensation/sumWeights) / (sumWeights - 1)
-	return
+
+	return v, nil
+}
+
+
+// _variance finds the variance for both population and sample data
+func _variance(input []float64, sample int) (variance float64, err error) {
+
+	if len(input) == 0 {
+		return math.NaN(), EmptyInputErr
+	}
+
+	// Sum the square of the mean subtracted from each number
+	m, _ := Mean(input)
+
+	for _, n := range input {
+		variance += (n - m) * (n - m)
+	}
+
+	// When getting the mean of the squared differences
+	// "sample" will allow us to know if it's a sample
+	// or population and wether to subtract by one or not
+	return variance / float64((len(input) - (1 * sample))), nil
+}
+
+
+// Mean gets the average of a slice of numbers
+func Mean(input []float64) (float64, error) {
+
+	if len(input) == 0 {
+		return math.NaN(), EmptyInputErr
+	}
+
+	sum := 0.0
+	for _, value := range input {
+		sum += value
+	}
+
+	return sum / float64(len(input)), nil
 }
