9a346ad741cbed4509a6e269dda5ee02f118542f
Raymond Luo <rymdluo@users.noreply.github.com>
2020-04-30 15:28:50 +0800

    asdaf

--- quanatee/contrib/pentaggregation/aggtrigger/functions/avgl.go
+++ quanatee/contrib/pentaggregation/aggtrigger/functions/avgl.go
@@ -9,8 +9,6 @@ func AvgLeftFloat32(values []float32) float32 {
 		avg += (val/float32(idx+1))
 	}
     
-    avg = avg/float32(length)
-    
 	return avg
 }
 
@@ -18,11 +16,8 @@ func AvgLeftFloat64(values []float64) float64 {
 	avg := big.NewFloat(float64(0.0))
 	length := len(values)
 	for idx, val := range values {
-        avg = avg.Add(avg, big.NewFloat(float64(val)))
         avg = avg.Add(avg, big.NewFloat(float64(val/float64(idx+1))))
 	}
-
-    avg = new(big.Float).Quo(avg, big.NewFloat(float64(length)))
 
     result, _ := avg.Float64()
     
--- quanatee/contrib/pentaggregation/aggtrigger/functions/avgr.go
+++ quanatee/contrib/pentaggregation/aggtrigger/functions/avgr.go
@@ -9,8 +9,6 @@ func AvgRightFloat32(values []float32) float32 {
 		avg += (val/float32(length-(idx+1)))
 	}
     
-    avg = avg/float32(length)
-    
 	return avg
 }
 
@@ -18,12 +16,9 @@ func AvgRightFloat64(values []float64) float64 {
 	avg := big.NewFloat(float64(0.0))
 	length := len(values)
 	for idx, val := range values {
-        avg = avg.Add(avg, big.NewFloat(float64(val)))
         avg = avg.Add(avg, big.NewFloat(float64((val/float64(length-(idx+1))))))
 	}
-
-    avg = new(big.Float).Quo(avg, big.NewFloat(float64(length)))
-
+	
     result, _ := avg.Float64()
     
 	return result
