742c867d51a2bbc6eafdd5c67ee20eca5f575796
Raymond Luo <rymdluo@users.noreply.github.com>
2020-05-01 00:11:05 +0800

    Update avgl.go

--- quanatee/contrib/pentaggregation/aggtrigger/functions/avgl.go
+++ quanatee/contrib/pentaggregation/aggtrigger/functions/avgl.go
@@ -30,7 +30,7 @@ func AvgLeftFloat64(values []float64) float64 {
 	decay := 2 / (float64(len(values)/2) + 1)
 	
 	for i := len(values)-1; i >= 0; i-- {
-		zsc := math.Abs(float64((val-avg)/std))
+		zsc := math.Abs(float64((values[i]-avg)/std))
 		// 95% Confidence Interval
 		if zsc < 1.960 {
 			e.Add(float64(values[i]), decay)
