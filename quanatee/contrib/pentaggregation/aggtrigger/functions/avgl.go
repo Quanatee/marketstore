4c8077b3d1df6595152331831b3952915a0b88f0
Raymond Luo <rymdluo@users.noreply.github.com>
2020-04-30 18:54:19 +0800

    dsaf

--- quanatee/contrib/pentaggregation/aggtrigger/functions/avgl.go
+++ quanatee/contrib/pentaggregation/aggtrigger/functions/avgl.go
@@ -1,23 +1,50 @@
 package functions
 
-import "math/big"
-
 func AvgLeftFloat32(values []float32) float32 {
-	avg := float32(0)
-	for idx, val := range values {
-		avg += (val/float32(idx+1))
+
+	var e SimpleEWMA
+	decay := 2 / (float64(len(values)) + 1)
+	
+	for i := len(values)-1; i >= 0; i-- {
+		e.Add(float64(values[i]), decay)
 	}
-    
-	return avg
+	
+	return float32(e.Value())
 }
 
 func AvgLeftFloat64(values []float64) float64 {
-	avg := big.NewFloat(float64(0.0))
-	for idx, val := range values {
-        avg = avg.Add(avg, big.NewFloat(float64(val/float64(idx+1))))
+	
+	var e SimpleEWMA
+	decay := 2 / (float64(len(values)) + 1)
+	
+	for i := len(values)-1; i >= 0; i-- {
+		e.Add(float64(values[i]), decay)
 	}
+	
+	return e.Value()
+}
 
-    result, _ := avg.Float64()
-    
-	return result
+type SimpleEWMA struct {
+	// The current value of the average. After adding with Add(), this is
+	// updated to reflect the average of all values seen thus far.
+	value float64
+}
+
+// Add adds a value to the series and updates the moving average.
+func (e *SimpleEWMA) Add(value, decay float64) {
+	if e.value == 0 { // this is a proxy for "uninitialized"
+		e.value = value
+	} else {
+		e.value = (value * decay) + (e.value * (1 - decay))
+	}
+}
+
+// Value returns the current value of the moving average.
+func (e *SimpleEWMA) Value() float64 {
+	return e.value
+}
+
+// Set sets the EWMA's value.
+func (e *SimpleEWMA) Set(value float64) {
+	e.value = value
 }
--- quanatee/contrib/pentaggregation/aggtrigger/functions/avgr.go
+++ quanatee/contrib/pentaggregation/aggtrigger/functions/avgr.go
@@ -1,25 +1,50 @@
 package functions
 
-import "math/big"
-
 func AvgRightFloat32(values []float32) float32 {
-	avg := float32(0)
-	length := len(values)
-	for idx, val := range values {
-		avg += (val/float32(length-(idx+1)))
+
+	var e SimpleEWMA
+	decay := 2 / (float64(len(values)) + 1)
+	
+	for _, val := range values {
+		e.Add(float64(val), decay)
 	}
-    
-	return avg
+	
+	return float32(e.Value())
 }
 
 func AvgRightFloat64(values []float64) float64 {
-	avg := big.NewFloat(float64(0.0))
-	length := len(values)
-	for idx, val := range values {
-        avg = avg.Add(avg, big.NewFloat(float64((val/float64(length-(idx+1))))))
+	
+	var e SimpleEWMA
+	decay := 2 / (float64(len(values)) + 1)
+	
+	for _, val := range values {
+		e.Add(float64(val), decay)
 	}
 	
-    result, _ := avg.Float64()
-    
-	return result
+	return e.Value()
+}
+
+type SimpleEWMA struct {
+	// The current value of the average. After adding with Add(), this is
+	// updated to reflect the average of all values seen thus far.
+	value float64
+}
+
+// Add adds a value to the series and updates the moving average.
+func (e *SimpleEWMA) Add(value, decay float64) {
+	if e.value == 0 { // this is a proxy for "uninitialized"
+		e.value = value
+	} else {
+		e.value = (value * decay) + (e.value * (1 - decay))
+	}
+}
+
+// Value returns the current value of the moving average.
+func (e *SimpleEWMA) Value() float64 {
+	return e.value
+}
+
+// Set sets the EWMA's value.
+func (e *SimpleEWMA) Set(value float64) {
+	e.value = value
 }
