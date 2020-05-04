fd838ae5606c1ef6ad309dee69b83db297626e94
Raymond Luo <rymdluo@users.noreply.github.com>
2020-05-05 01:52:25 +0800

    dsaf

--- quanatee/contrib/pentagon/api4polygon/api.go
+++ quanatee/contrib/pentagon/api4polygon/api.go
@@ -204,7 +204,7 @@ func GetAggregates(
 	for bar := 0; bar < length; bar++ {
 		if len(agg.PriceData) <= bar {
 			// Unknown issue that causes index out of range
-			log.Info("%s bar went too far %v/%v", bar, length)
+			log.Info("[polygon] %s bar went too far %v/%v", symbol, bar, length)
 			break
 		}
 		if agg.PriceData[bar].Open != 0 && agg.PriceData[bar].High != 0 && agg.PriceData[bar].Low != 0 && agg.PriceData[bar].Close != 0 {
--- quanatee/contrib/pentagon/api4tiingo/api.go
+++ quanatee/contrib/pentagon/api4tiingo/api.go
@@ -169,6 +169,7 @@ func GetAggregates(
 		if strings.Compare(marketType, "crypto") == 0 {
 			if len(aggCrypto[0].PriceData) <= bar {
 				// Unknown issue that causes index out of range
+				log.Info("[tiingo] %s bar went too far %v/%v", symbol, bar, length)
 				break
 			}
 			dt, err_dt := time.Parse(time.RFC3339, aggCrypto[0].PriceData[bar].Date)
@@ -270,7 +271,8 @@ func GetAggregates(
 			}
 		} else if strings.Compare(marketType, "forex") == 0 {
 			if len(aggForex.PriceData) <= bar {
-				// Unknown issue that causes index out of range (Probably malformed json)
+				// Unknown issue that causes index out of range
+				log.Info("[tiingo] %s bar went too far %v/%v", symbol, bar, length)
 				break
 			}
 			dt, err_dt := time.Parse(time.RFC3339, aggForex.PriceData[bar].Date)
@@ -367,6 +369,7 @@ func GetAggregates(
 			}
 		} else if strings.Compare(marketType, "equity") == 0 {
 			if len(aggEquity.PriceData) <= bar {
+				log.Info("[tiingo] %s bar went too far %v/%v", symbol, bar, length)
 				break
 			}
 			dt, err_dt := time.Parse(time.RFC3339, aggEquity.PriceData[bar].Date)
