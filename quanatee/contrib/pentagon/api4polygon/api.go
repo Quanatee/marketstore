ed49370d210c5f4c57ad3e795e56566bce1ee59d
Raymond Luo <rymdluo@users.noreply.github.com>
2020-05-05 00:32:17 +0800

    Update api.go

--- quanatee/contrib/pentagon/api4polygon/api.go
+++ quanatee/contrib/pentagon/api4polygon/api.go
@@ -66,7 +66,7 @@ func UpdateSplitEvents(symbol string, timeStarted time.Time) (bool) {
 
 	if len(splitsItem.SplitData) > 0 {
 		
-		symbolSplits, ok := SplitEvents.Load(symbol)
+		symbolSplits, ok := api.SplitEvents.Load(symbol)
 		
 		if ok == false {
 			// First time
@@ -76,7 +76,7 @@ func UpdateSplitEvents(symbol string, timeStarted time.Time) (bool) {
 				symbolSplits[expiryDate] = splitData.Ratio
 			}
 			if len(symbolSplits) > 0 {
-				SplitEvents.Store(symbol, symbolSplits)
+				api.SplitEvents.Store(symbol, symbolSplits)
 				log.Info("[polygon] %s: %v", symbol, symbolSplits)
 			}
 		} else {
@@ -85,7 +85,7 @@ func UpdateSplitEvents(symbol string, timeStarted time.Time) (bool) {
 			for _, splitData := range splitsItem.SplitData {
 				expiryDate, _ := time.Parse("2006-01-02", splitData.Expiry)
 				if _, ok := symbolSplits[expiryDate]; ok {
-					upcomingSplit, _ := UpcomingSplitEvents.Load(symbol)
+					upcomingSplit, _ := api.UpcomingSplitEvents.Load(symbol)
 					if upcomingSplit != nil {
 						upcomingExpiryDate := upcomingSplit.(time.Time)
 						// There are two ways to trigger a backfill
@@ -96,17 +96,17 @@ func UpdateSplitEvents(symbol string, timeStarted time.Time) (bool) {
 							( expiryDate.After(timeStarted) || (expiryDate.Day() == time.Now().Day() && expiryDate.Month() == time.Now().Month() && expiryDate.Year() == time.Now().Year()) ) ) {
 							rebackfill = true
 							// Deregister as an upcoming split event
-							UpcomingSplitEvents.Store(symbol, nil)
+							api.UpcomingSplitEvents.Store(symbol, nil)
 						}
 					}
 				} else {
 					// New split event detected, we only store 1 upcoming split event per symbol at any given time
 					symbolSplits[expiryDate] = splitData.Ratio
-					UpcomingSplitEvents.Store(symbol, expiryDate)
+					api.UpcomingSplitEvents.Store(symbol, expiryDate)
 				}
 			}
 			if len(symbolSplits) > 0 {
-				SplitEvents.Store(symbol, symbolSplits)
+				api.SplitEvents.Store(symbol, symbolSplits)
 			}
 		}
 	}
