361138782fb0ae35247cd469dbe3905727b52d14
Raymond Luo <rymdluo@users.noreply.github.com>
2020-05-07 02:15:56 +0800

    Update api.go

--- quanatee/contrib/pentagon/api/api.go
+++ quanatee/contrib/pentagon/api/api.go
@@ -235,13 +235,11 @@ func aggregate(cs *io.ColumnSeries, tbk *io.TimeBucketKey) *io.ColumnSeries {
 	timeWindow := utils.CandleDurationFromString(tbk.GetItemInCategory("Timeframe"))
 	
 	groupKey := timeWindow.Truncate(ts[0])
-	log.Info("%v", ts)
-	log.Info("%v", groupKey)
 	groupStart := 0
 	// accumulate inputs.  Since the input is ordered by
 	// time, it is just to slice by correct boundaries
 	for i, t := range ts {
-		if !timeWindow.IsWithin(t, groupKey) {
+		if timeWindow.IsWithin(t, groupKey) {
 			// Emit new row and re-init aggState
 			outEpoch = append(outEpoch, groupKey.Unix())
 			accumGroup.apply(groupStart, i)
