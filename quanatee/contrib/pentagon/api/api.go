b766c4aa2aa9f573790824ddf272fbfd6558a229
Raymond Luo <rymdluo@users.noreply.github.com>
2020-05-07 19:32:00 +0800

    Update api.go

--- quanatee/contrib/pentagon/api/api.go
+++ quanatee/contrib/pentagon/api/api.go
@@ -287,11 +287,7 @@ func aggregate(cs *io.ColumnSeries, tbk *io.TimeBucketKey) *io.ColumnSeries {
 			if t.Hour() >= 20 && t.Hour() <= 22 {
 				log.Info("%v: %v-%v (%v-%v)", tbk.String, groupStart, i, ts[groupStart], ts[i])
 			}
-			if len(ts) > i+1{
-				groupStart = i+1
-			} else {
-				break
-			}
+			groupStart = i
 		}
 	}
 	// accumulate any remaining values if not yet
