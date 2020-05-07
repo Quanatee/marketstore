81a194f87c1a17297334833c95ea5830e758b8ed
Raymond Luo <rymdluo@users.noreply.github.com>
2020-05-07 16:17:28 +0800

    Update filler.go

--- quanatee/contrib/pentagon/filler/filler.go
+++ quanatee/contrib/pentagon/filler/filler.go
@@ -35,29 +35,22 @@ func Bars(wg *sync.WaitGroup, symbol, marketType string, from, to time.Time, tim
 		to = time.Now()
 	}
 	
-	var ohlcvs []api.OHLCV
-	var sources []string
+	var ohlcvs map[string]api.OHLCV
 
 	ohlcv := GetDataFromProvider("polygon", symbol, marketType, from, to)
 	if len(ohlcv.HLC) > 0 {
-		ohlcvs = append(ohlcvs, ohlcv)
-		sources = append(sources, "polygon")
-		log.Debug("Adding Polygon %s A %v", symbol, len(ohlcvs))
+		ohlcvs["polygon"] = ohlcv
 	}
 	ohlcv_ti := GetDataFromProvider("tiingo", symbol, marketType, from, to)
 	if len(ohlcv_ti.HLC) > 0 {
-		ohlcvs = append(ohlcvs, ohlcv_ti)
-		sources = append(sources, "tiingo")
-		log.Debug("Adding Tiingo %s R %v", symbol, len(ohlcvs))
+		ohlcvs["tiingo"] = ohlcv_ti
 	}
 	// Randomly run alt providers to ease api usage
 	rand.Seed(GetRandSeed())
 	if rand.Intn(3) <= 1 {
 		ohlcv_tw := GetDataFromProvider("twelve", symbol, marketType, from, to)
 		if len(ohlcv_tw.HLC) > 0 {
-			ohlcvs = append(ohlcvs, ohlcv_tw)
-			sources = append(sources, "twelve")
-			log.Debug("Adding Twelve %s R %v", symbol, len(ohlcvs))
+			ohlcvs["twelve"] = ohlcv_tw
 		}
 	}
 	
@@ -66,85 +59,66 @@ func Bars(wg *sync.WaitGroup, symbol, marketType string, from, to time.Time, tim
 		// BUSD
 		ohlcv_pgb := GetDataFromProvider("polygon", symbol[:len(symbol)-3] + "B" + symbol[len(symbol)-3:], marketType, from, to)
 		if len(ohlcv_pgb.HLC) > 0 {
-			ohlcvs = append(ohlcvs, ohlcv_pgb)
-			log.Debug("Adding Polygon BUSD %s to %v", symbol, len(ohlcvs))
+			ohlcvs["polygon_busd"] = ohlcv_pgb
 		}
 		rand.Seed(GetRandSeed())
 		if rand.Intn(2) <= 1 {
 			ohlcv_tib := GetDataFromProvider("tiingo", symbol[:len(symbol)-3] + "B" + symbol[len(symbol)-3:], marketType, from, to)
 			if len(ohlcv_tib.HLC) > 0 {
-				ohlcvs = append(ohlcvs, ohlcv_tib)
-				log.Debug("Adding Tiingo BUSD %s to %v", symbol, len(ohlcvs))
+				ohlcvs["tiingo_busd"] = ohlcv_tib
 			}
 		} else {
 			ohlcv_twb := GetDataFromProvider("twelve", symbol[:len(symbol)-3] + "B" + symbol[len(symbol)-3:], marketType, from, to)
 			if len(ohlcv_twb.HLC) > 0 {
-				ohlcvs = append(ohlcvs, ohlcv_twb)
-				sources = append(sources, "twelve")
-				log.Debug("Adding Twelve BUSD %s to %v", symbol, len(ohlcvs))
+				ohlcvs["twelve_busd"] = ohlcv_twb
 			}
 		}
 		// USDT
 		ohlcv_pgt := GetDataFromProvider("polygon", symbol+"T", marketType, from, to)
 		if len(ohlcv_pgt.HLC) > 0 {
-			ohlcvs = append(ohlcvs, ohlcv_pgt)
-			sources = append(sources, "polygon")
-			log.Debug("Adding Polygon USDT %s to %v", symbol, len(ohlcvs))
+			ohlcvs["polygon_usdt"] = ohlcv_pgt
 		}
 		rand.Seed(GetRandSeed())
 		if rand.Intn(2) <= 1 {
 			ohlcv_tit := GetDataFromProvider("tiingo", symbol+"T", marketType, from, to)
 			if len(ohlcv_tit.HLC) > 0 {
-				ohlcvs = append(ohlcvs, ohlcv_tit)
-				sources = append(sources, "tiingo")
-				log.Debug("Adding Tiingo USDT %s to %v", symbol, len(ohlcvs))
+				ohlcvs["tiingo_usdt"] = ohlcv_tit
 			}
 		} else {
 			ohlcv_twt := GetDataFromProvider("twelve", symbol+"T", marketType, from, to)
 			if len(ohlcv_twt.HLC) > 0 {
-				ohlcvs = append(ohlcvs, ohlcv_twt)
-				sources = append(sources, "twelve")
-				log.Debug("Adding Twelve USDT %s to %v", symbol, len(ohlcvs))
+				ohlcvs["twelve_usdt"] = ohlcv_twt
 			}
 		}
 		// USDC
 		ohlcv_pgc := GetDataFromProvider("polygon", symbol+"C", marketType, from, to)
 		if len(ohlcv_pgc.HLC) > 0 {
-			ohlcvs = append(ohlcvs, ohlcv_pgc)
-			sources = append(sources, "polygon")
-			log.Debug("Adding Polygon USDC %s to %v", symbol, len(ohlcvs))
+			ohlcvs["polygon_usdc"] = ohlcv_pgc
 		}
 		rand.Seed(GetRandSeed())
 		if rand.Intn(2) <= 1 {
 			ohlcv_tic := GetDataFromProvider("tiingo", symbol+"C", marketType, from, to)
 			if len(ohlcv_tic.HLC) > 0 {
-				ohlcvs = append(ohlcvs, ohlcv_tic)
-				sources = append(sources, "tiingo")
-				log.Debug("Adding Tiingo USDC %s to %v", symbol, len(ohlcvs))
+				ohlcvs["tiingo_usdc"] = ohlcv_tic
 			}
 		} else {
 			ohlcv_twc := GetDataFromProvider("twelve", symbol+"C", marketType, from, to)
 			if len(ohlcv_twc.HLC) > 0 {
-				ohlcvs = append(ohlcvs, ohlcv_twc)
-				sources = append(sources, "twelve")
-				log.Debug("Adding Twelve USDC %s to %v", symbol, len(ohlcvs))
+				ohlcvs["twelve_usdc"] = ohlcv_twc
 			}
 		}
 	}
-
+	
 	// Get the Epoch slice of the largest OHLCV set
 	Epochs_ := make([]int64, 0)
 	
     for _, ohlcv_ := range ohlcvs {
-		if len(ohlcv_.HLC) > len(Epochs_) {
-			// Epochs_ = make([]string, 0, len(len(ohlcv_.HLC)))
-			for key := range ohlcv_.HLC {
-				Epochs_ = append(Epochs_, key)
-			}
+		for key := range ohlcv_.HLC {
+			Epochs_ = append(Epochs_, key)
 		}
 	}
-
-	// If length is 0, no data was returned
+	
+	Epochs_ = removeDuplicatesInt64(Epochs_)
 	if len(Epochs_) == 0 {
 		return
 	}
@@ -169,23 +143,39 @@ func Bars(wg *sync.WaitGroup, symbol, marketType string, from, to time.Time, tim
 				}
 			}
 		}
-		for _, ohlcv_ := range ohlcvs {
+		for source, ohlcv_ := range ohlcvs {
 			if ( (ohlcv_.Open[Epoch] != 0 && ohlcv_.High[Epoch] != 0 && ohlcv_.Low[Epoch] != 0 && ohlcv_.Close[Epoch] != 0) &&
 				(ohlcv_.Volume[Epoch] != 0) &&
 				(ohlcv_.HLC[Epoch] != 0) &&
 				(ohlcv_.TVAL[Epoch] != 0) &&
 				(ohlcv_.Spread[Epoch] != 0) ) {
-				open += float32(ohlcv_.Open[Epoch] / split)
-				high += float32(ohlcv_.High[Epoch] / split)
-				low += float32(ohlcv_.Low[Epoch] / split)
-				close += float32(ohlcv_.Close[Epoch] / split)
-				hlc += float32(ohlcv_.HLC[Epoch] / split)
-				spread += float32(ohlcv_.Spread[Epoch] / split)
-				divisor += float32(1)
-				if ohlcv_.Volume[Epoch] > 1 {
-					volume += float32(ohlcv_.Volume[Epoch] * split)
-					tval += float32(ohlcv_.TVAL[Epoch])
-					volume_divisor += float32(1)
+				
+				if strings.Contains(source, "polygon") || strings.Contains(source, "twelve")  {
+					open += float32(ohlcv_.Open[Epoch] * split)
+					high += float32(ohlcv_.High[Epoch] * split)
+					low += float32(ohlcv_.Low[Epoch] * split)
+					close += float32(ohlcv_.Close[Epoch] * split)
+					hlc += float32(ohlcv_.HLC[Epoch] * split)
+					spread += float32(ohlcv_.Spread[Epoch] * split)
+					divisor += float32(1)
+					if ohlcv_.Volume[Epoch] > 1 {
+						volume += float32(ohlcv_.Volume[Epoch] / split)
+						tval += float32(ohlcv_.TVAL[Epoch])
+						volume_divisor += float32(1)
+					}
+				} else {
+					open += float32(ohlcv_.Open[Epoch])
+					high += float32(ohlcv_.High[Epoch])
+					low += float32(ohlcv_.Low[Epoch])
+					close += float32(ohlcv_.Close[Epoch])
+					hlc += float32(ohlcv_.HLC[Epoch])
+					spread += float32(ohlcv_.Spread[Epoch])
+					divisor += float32(1)
+					if ohlcv_.Volume[Epoch] > 1 {
+						volume += float32(ohlcv_.Volume[Epoch])
+						tval += float32(ohlcv_.TVAL[Epoch])
+						volume_divisor += float32(1)
+					}
 				}
 			}
 		}
@@ -209,9 +199,9 @@ func Bars(wg *sync.WaitGroup, symbol, marketType string, from, to time.Time, tim
 	}
 	
 	if (to.Add(5*time.Minute)).After(time.Now()) {
-		log.Debug("filler.Bars(%s) livefill via %v [from %v to %v] | Length(%v)", symbol, removeDuplicatesUnordered(sources), from, to, len(Epochs))
+		log.Debug("filler.Bars(%s) livefill via %v [from %v to %v] | Length(%v)", symbol, removeDuplicatesString(sources), from, to, len(Epochs))
 	} else {
-		log.Info("filler.Bars(%s) backfill via %v [from %v to %v] | Length(%v)", symbol, removeDuplicatesUnordered(sources), from, to, len(Epochs))
+		log.Info("filler.Bars(%s) backfill via %v [from %v to %v] | Length(%v)", symbol, removeDuplicatesString(sources), from, to, len(Epochs))
 	}
 	
 	cs := io.NewColumnSeries()
@@ -317,8 +307,21 @@ func GetRandSeed() (int64) {
 	}
 	return int64(binary.LittleEndian.Uint64(b[:]))
 }
+func removeDuplicatesInt64(s []int64) []int64 {
+	seen := make(map[int64]struct{}, len(s))
+	j := 0
+	for _, v := range s {
+		if _, ok := seen[v]; ok {
+			continue
+		}
+		seen[v] = struct{}{}
+		s[j] = v
+		j++
+	}
+	return s[:j]
+}
 
-func removeDuplicatesUnordered(elements []string) []string {
+func removeDuplicatesString(elements []string) []string {
     encountered := map[string]bool{}
 
     // Create a map of all unique elements.
