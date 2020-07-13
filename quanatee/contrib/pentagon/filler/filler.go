e7034045e92e938deaa2978ea5cf80c00899fdb6
Raymond Luo <rymdluo@users.noreply.github.com>
2020-07-14 04:05:51 +0800

    Update filler.go

--- quanatee/contrib/pentagon/filler/filler.go
+++ quanatee/contrib/pentagon/filler/filler.go
@@ -191,6 +191,7 @@ func GetDataFromProvider(
 		if err != nil {
 			log.Error("[polygon] %s %s bars from: %v to %v failure: (%v)", symbol, filltype, from, to, err)
 		} else {
+			log.Info("GetDataFromProvider(%s) backfill via %s [from %v to %v] | Length(%v)", symbol, provider, from, to, len(ohlcv.HLC))
 			if len(ohlcv.HLC) > 0 {
 				reconstruct := api.OHLCV{
 					Open: ohlcv.Open,
@@ -211,6 +212,7 @@ func GetDataFromProvider(
 		if err != nil {
 			log.Error("[tiingo] %s %s bars from: %v to %v failure: (%v)", symbol, filltype, from, to, err)
 		} else {
+			log.Info("GetDataFromProvider(%s) backfill via %s [from %v to %v] | Length(%v)", symbol, provider, from, to, len(ohlcv.HLC))
 			if len(ohlcv.HLC) > 0 {
 				reconstruct := api.OHLCV{
 					Open: ohlcv.Open,
@@ -230,6 +232,7 @@ func GetDataFromProvider(
 		if err != nil {
 			log.Error("[twelve] %s %s bars from: %v to %v failure: (%v)", symbol, filltype, from, to, err)
 		} else {
+			log.Info("GetDataFromProvider(%s) backfill via %s [from %v to %v] | Length(%v)", symbol, provider, from, to, len(ohlcv.HLC))
 			if len(ohlcv.HLC) > 0 {
 				reconstruct := api.OHLCV{
 					Open: ohlcv.Open,
